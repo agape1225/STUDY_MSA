8c4a831a48b77b9d9516f7bd98050abffbbf8f56
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-06-06 02:21:46 +0900

    update crud

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.userservice.controller;
 
 import com.example.userservice.VO.Greeting;
+import com.example.userservice.VO.RequestUser;
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -13,10 +17,12 @@ public class UserController {
 
     private Environment env;
     private Greeting greeting;
+    private UserService userService;
     @Autowired
-    public UserController(Environment env, Greeting greeting){
+    public UserController(Environment env, Greeting greeting, UserService userService){
         this.env = env;
         this.greeting = greeting;
+        this.userService = userService;
     }
 
     @GetMapping("/heath_check")
@@ -31,6 +37,18 @@ public class UserController {
     public String welcome(){
 
         return greeting.getMessage();
+
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        return "Create user method is called";
 
     }
 }
